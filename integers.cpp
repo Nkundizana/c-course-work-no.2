c3dc353f986f27a343c4d4dc98daba9513241e4e
Nkundizana <135699924+Nkundizana@users.noreply.github.com>
2024-09-01 10:03:42 +0300

    Add files via upload

---
+++ integers.cpp
@@ -0,0 +1,15 @@
+#include <iostream> 
+
+int main() {
+    int n;
+    std::cout << "Enter a positive integer n: ";
+    std::cin >> n;
+    std::cout << "Divisors: ";
+    for (int i = 1; i <= n; i++) {
+        if (n % i == 0) {
+            std::cout << "\n" << i;
+        }
+    }
+    return 0;
+}
+
